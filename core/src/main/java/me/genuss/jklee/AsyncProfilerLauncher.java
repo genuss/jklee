96291e1b8bf1c8daed348d3daa4028613ec34ee4
Aleksey Genus <agenus@projectx019.com>
2023-10-20 16:01:39 +0200

    Fix sorting

--- core/src/main/java/me/genuss/jklee/AsyncProfilerLauncher.java
+++ core/src/main/java/me/genuss/jklee/AsyncProfilerLauncher.java
@@ -12,6 +12,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.time.Instant;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.Executors;
@@ -105,7 +106,7 @@ class AsyncProfilerLauncher {
       return stream
           .filter(not(resultsDir::equals))
           .map(this::toProfilingResult)
-          .sorted()
+          .sorted(Comparator.comparing(ProfilingResult::endedAt).reversed())
           .collect(Collectors.toList());
     } catch (IOException e) {
       return List.of();
