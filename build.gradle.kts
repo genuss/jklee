9a438ea2762bef7deaf612d33893d5cca60d8863
Alexey Genus <genus.alexey@gmail.com>
2024-12-30 13:41:51 +0100

    Don't releaser in branches

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,57 +7,6 @@ plugins {
 val stageRepoPath: String =
     rootProject.layout.projectDirectory.dir("staging-repo").asFile.absolutePath
 
-jreleaser {
-  signing {
-    active = org.jreleaser.model.Active.ALWAYS
-    armored = true
-    checksums = false
-    mode = org.jreleaser.model.Signing.Mode.MEMORY
-    verify = true
-  }
-  strict = true
-  project {
-    author("Alexey Genus")
-    description = "Jklee integrates async-profiler in spring-boot application"
-    inceptionYear = "2024"
-    versionPattern = "CUSTOM"
-  }
-  release {
-    github {
-      overwrite = true
-      prerelease {
-        enabled = true
-        pattern = ".+\\b[0-9a-f]{40}\\b.+"
-      }
-      sign = true
-      signatures = true
-      skipTag = true
-    }
-  }
-  deploy {
-    maven {
-      github {
-        create("jklee") {
-          active = org.jreleaser.model.Active.ALWAYS
-          applyMavenCentralRules = true
-          sign = true
-          stagingRepository(stageRepoPath)
-          url = "https://maven.pkg.github.com/genuss/jklee"
-        }
-      }
-      mavenCentral {
-        create("jklee") {
-          active = org.jreleaser.model.Active.ALWAYS
-          sign = true
-          stage = org.jreleaser.model.api.deploy.maven.MavenCentralMavenDeployer.Stage.UPLOAD
-          stagingRepository(stageRepoPath)
-          url = "https://central.sonatype.com/api/v1/publisher"
-        }
-      }
-    }
-  }
-}
-
 scmVersion {
   snapshotCreator { _, _ -> "" }
   versionIncrementer { ctx ->
@@ -82,6 +31,59 @@ scmVersion {
       return@versionCreator "$versionFromTag$runNumberSuffix$revisionSuffix$dirtySuffix"
     }
     "$versionFromTag$dirtySuffix"
+  }
+}
+
+jreleaser {
+  signing {
+    active = org.jreleaser.model.Active.ALWAYS
+    armored = true
+    checksums = false
+    mode = org.jreleaser.model.Signing.Mode.MEMORY
+    verify = true
+  }
+  strict = true
+  project {
+    author("Alexey Genus")
+    description = "Jklee integrates async-profiler in spring-boot application"
+    inceptionYear = "2024"
+    versionPattern = "CUSTOM"
+  }
+  release {
+    github {
+      overwrite = true
+      prerelease {
+        enabled = true
+        pattern = ".+\\b[0-9a-f]{40}\\b.+"
+      }
+      previousTagName = scmVersion.previousVersion
+      sign = true
+      signatures = true
+      skipTag = true
+      tagName = scmVersion.version
+    }
+  }
+  deploy {
+    maven {
+      github {
+        create("jklee") {
+          active = org.jreleaser.model.Active.ALWAYS
+          applyMavenCentralRules = true
+          sign = true
+          stagingRepository(stageRepoPath)
+          url = "https://maven.pkg.github.com/genuss/jklee"
+        }
+      }
+      mavenCentral {
+        create("jklee") {
+          active = org.jreleaser.model.Active.ALWAYS
+          sign = true
+          stage = org.jreleaser.model.api.deploy.maven.MavenCentralMavenDeployer.Stage.UPLOAD
+          stagingRepository(stageRepoPath)
+          url = "https://central.sonatype.com/api/v1/publisher"
+        }
+      }
+    }
   }
 }
 
