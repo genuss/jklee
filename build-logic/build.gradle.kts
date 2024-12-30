395c88ed3a3c25d13131b5f871ab326fede912fc
Alexey Genus <genus.alexey@gmail.com>
2024-12-30 14:27:13 +0100

    Set up versioning and artifacts upload
    Switch to pl.allegro.tech.build.axion-release gradle plugin instead of jgitver. The latter one is too old and doesn't work well with new gradle versions.
    Don't create releases for builds in branches. It's just useless and creates a myriad of tags.
    Don't upload branch builds in maven central

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -4,7 +4,6 @@ plugins {
 }
 
 dependencies {
-  implementation(libs.jgitver.gradlePlugin)
   implementation(libs.siouanFrontend.gradlePlugin)
   implementation(libs.spotless.gradlePlugin)
   implementation(libs.springBoot.gradlePlugin)
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,36 @@
 plugins {
   id("core")
   id("org.jreleaser") version "1.15.0"
+  id("pl.allegro.tech.build.axion-release") version "1.18.16"
 }
 
 val stageRepoPath: String =
     rootProject.layout.projectDirectory.dir("staging-repo").asFile.absolutePath
+
+scmVersion {
+  snapshotCreator { _, _ -> "" }
+  versionCreator { versionFromTag, position ->
+    if (!position.isClean &&
+        providers.environmentVariable("CI").map(String::toBoolean).getOrElse(false)) {
+      throw IllegalStateException("Cannot release dirty version in CI")
+    }
+    val revision =
+        providers
+            .environmentVariable("GITHUB_SHA")
+            .orElse(position.revision)
+            .map { it.substring(0, 7) }
+            .getOrElse("unknown")
+    val revisionSuffix = "-$revision"
+    val dirtySuffix = if (position.isClean) "" else "-dirty"
+    val runNumber = providers.environmentVariable("GITHUB_RUN_NUMBER").getOrElse("000000")
+    val runNumberSuffix = "-${runNumber.padStart(6, '0')}"
+
+    if (position.branch != "master") {
+      return@versionCreator "$versionFromTag$runNumberSuffix$revisionSuffix$dirtySuffix"
+    }
+    "$versionFromTag$dirtySuffix"
+  }
+}
 
 jreleaser {
   signing {
@@ -14,7 +40,6 @@ jreleaser {
     mode = org.jreleaser.model.Signing.Mode.MEMORY
     verify = true
   }
-  gitRootSearch = true
   strict = true
   project {
     author("Alexey Genus")
@@ -24,11 +49,11 @@ jreleaser {
   }
   release {
     github {
-      version = "0.0.0-dev"
       overwrite = true
-      signatures = true
-      update { skipTag = true }
+      previousTagName = scmVersion.previousVersion
       sign = true
+      signatures = true
+      tagName = scmVersion.version
     }
   }
   deploy {
@@ -39,13 +64,16 @@ jreleaser {
           applyMavenCentralRules = true
           sign = true
           stagingRepository(stageRepoPath)
-          url = "https://maven.pkg.github.com/genuss/jklee"
+          url =
+              providers
+                  .environmentVariable("GITHUB_REPOSITORY")
+                  .map { "https://maven.pkg.github.com/$it" }
+                  .getOrElse("https://maven.pkg.github.com/genuss/jklee")
         }
       }
       mavenCentral {
         create("jklee") {
           active = org.jreleaser.model.Active.ALWAYS
-          sign = true
           stage = org.jreleaser.model.api.deploy.maven.MavenCentralMavenDeployer.Stage.UPLOAD
           stagingRepository(stageRepoPath)
           url = "https://central.sonatype.com/api/v1/publisher"
@@ -53,4 +81,13 @@ jreleaser {
       }
     }
   }
+}
+
+group = "me.genuss.jklee"
+
+version = scmVersion.version
+
+allprojects {
+  project.group = rootProject.group
+  project.version = rootProject.version
 }
