81859a715d9c015e6c87d8032766cf393ddef7b6
Alexey Genus <genus.alexey@gmail.com>
2024-12-30 10:29:20 +0100

    Try yml config

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,54 +3,7 @@ plugins {
   id("org.jreleaser") version "1.15.0"
 }
 
-val stageRepoPath: String =
-    rootProject.layout.projectDirectory.dir("staging-repo").asFile.absolutePath
-
 jreleaser {
-  signing {
-    active = org.jreleaser.model.Active.ALWAYS
-    armored = true
-    checksums = false
-    mode = org.jreleaser.model.Signing.Mode.MEMORY
-    verify = true
-  }
-  gitRootSearch = true
+  configFile = layout.projectDirectory.file("jreleaser.yml")
   strict = true
-  project {
-    author("Alexey Genus")
-    description = "Jklee integrates async-profiler in spring-boot application"
-    inceptionYear = "2024"
-    versionPattern = "CUSTOM"
-  }
-  release {
-    github {
-      overwrite = true
-      prerelease { enabled = true }
-      signatures = true
-      update { skipTag = true }
-      sign = true
-    }
-  }
-  deploy {
-    maven {
-      github {
-        create("jklee") {
-          active = org.jreleaser.model.Active.ALWAYS
-          applyMavenCentralRules = true
-          sign = true
-          stagingRepository(stageRepoPath)
-          url = "https://maven.pkg.github.com/genuss/jklee"
-        }
-      }
-      mavenCentral {
-        create("jklee") {
-          active = org.jreleaser.model.Active.RELEASE
-          sign = true
-          stage = org.jreleaser.model.api.deploy.maven.MavenCentralMavenDeployer.Stage.UPLOAD
-          stagingRepository(stageRepoPath)
-          url = "https://central.sonatype.com/api/v1/publisher"
-        }
-      }
-    }
-  }
 }
